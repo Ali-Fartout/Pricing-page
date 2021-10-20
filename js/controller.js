0f8b1b8cd67f078c36ab05e12479d73d3dd8f074
Ali Fartout <alifa2080@gmail.com>
2021-10-20 10:28:52 +0330

    remain: fix js fucntion, fix background, fix responsibility

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,30 @@
+priceList = {
+    '$ 19.99':'$ 199.99',
+    '$ 24.99':'$ 249.99',
+    '$ 39.99':'$ 399.99',
+    '$ 199.99':'$ 19.99',
+    '$ 249.99':'$ 24.99',
+    '$ 399.99':'$ 39.99'
+}
+
 $('document').ready(function () {
     $('input[name=checkbox]').change(function() {
         if ($(this).is(':checked')) {
            $("p[class=price]").each(function (){
-               $fragments.append($(this).html())
-               $(this).html()
-
+               var $price =  $(this).html();
+               if  (priceList.hasOwnProperty($price)) {
+                   $(this).html(priceList[$price])
+               }
            });
         } else {
-
+            var $price =  $(this).html();
+            $(this).html(priceList[$price])
+            $("p[class=price]").each(function (){
+                var $price =  $(this).html();
+                if  (priceList.hasOwnProperty($price)) {
+                    $(this).html(priceList[$price])
+                }
+            });
         }
     });
 });
